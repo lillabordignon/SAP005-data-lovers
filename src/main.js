24dafda907a93a95a2009f595a9c06b8367c913f
Kauana Maria Agostini <kauanaagostini@gmail.com>
2020-11-25 14:03:20 -0300

    criado ordenações da pagina pokemon.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import {
-  example
-} from './data.js';
+import {ordenarAZ, ordenarZA, ordenarType, ordenarRarity} from './data.js';
 import data from './data/pokemon/pokemon.js';
 
 
-console.log(example, data);
+//console.log(example, data);
 
 const showCards = () => {
   for (let pokemon of data.pokemon) {
@@ -24,19 +22,48 @@ window.addEventListener('load', () => {
 const btnsearch = document.getElementById("btnsearch")
 const input = document.getElementById("search")
 
-function pokemonSearch(){
-    let cards = document.querySelector('.c-cards')
+function pokemonCardsClear(){
+  let cards = document.querySelector('.c-cards')
     while (cards.firstChild){
         cards.removeChild(cards.firstChild);
     }
+}
+
+function pokemonSearch(){
+    pokemonCardsClear()
     data.pokemon.filter((pokemon) =>{
         const search = document.getElementById("search").value
         if (pokemon.name.startsWith(search)){
-            let cards = document.querySelector('.c-cards');
-            let card = document.createElement('section');
-            cards.appendChild(card);
-            card.innerHTML = `<section class="card"><img src="${pokemon.img}" alt=""><h1>${pokemon.name}</h1><button>Detalhes</button></section>`;
+          let cards = document.querySelector('.c-cards');
+          let card = document.createElement('section');
+          cards.appendChild(card);
+          card.innerHTML = card.innerHTML = `<article class="card type-${pokemon.type[0]}"><span class='card-number'>${pokemon.num}</span><img src="${pokemon.img}" alt=""><h1>${pokemon.name}</h1><h2>${pokemon.type}</h2><button>Detalhes</button></article>`;
         }
     })
 }
 btnsearch.addEventListener("click", pokemonSearch)
+
+const pokemonOrder = document.getElementById("ordenar")
+pokemonOrder.addEventListener("change", qualquer)
+function qualquer(){
+  const index = pokemonOrder.selectedIndex;
+  if (index == 1){
+    pokemonCardsClear();
+    showCards(ordenarAZ(data.pokemon))
+  } if (index == 2){
+    pokemonCardsClear();
+    showCards(ordenarZA(data.pokemon))
+  } if (index == 3){
+    pokemonCardsClear();
+    showCards(ordenarType(data.pokemon))
+  } if (index == 4){
+    pokemonCardsClear();
+    showCards(ordenarRarity(data.pokemon))
+  } else {
+    showCards(data.pokemon)
+  }
+  console.log(index)
+}
+
+console.log(data.pokemon[1])
+
