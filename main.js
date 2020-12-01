0d60e31de24d380a9eea8465e7194c4a06672e11
Kauana Maria Agostini <kauanaagostini@gmail.com>
2020-12-01 14:52:28 -0300

    merge das modificações dos dois arquivos

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {ordenarAZ, ordenarZA, ordenarType, ordenarNum} from './data.js';
+import {ordenarAZ, ordenarZA, ordenarType, ordenarNum, getBaseAttackPercentage} from './data.js';
 import data from './data/pokemon/pokemon.js';
 
 
@@ -10,32 +10,42 @@ const showCards = () => {
     let card = document.createElement('section');
     cards.appendChild(card);
     card.innerHTML = `<article class="card type-${pokemon.type[0]}" data-num="${pokemon.num}"><span class='card-number'>${pokemon.num}</span><img src="${pokemon.img}" alt=""><h1>${pokemon.name}</h1><h2>${pokemon.type}</h2><button>Detalhes</button></article>`;
+    let cardsButtons = document.querySelectorAll('.card button');
+    cardsButtons.forEach(button => {
+      button.addEventListener('click', showModal);
+    });
   }
 }
 
 const showModal = (event) => {
-
   modal.classList.add('itemActive');
-
   let contentModal = modal.querySelector('.c-modal_content');
   contentModal.removeAttribute('id');
-  
   let elClick = event.target;
   let cardClicked = elClick.closest('article.card');
-
   let pokeNum = cardClicked.dataset.num;
   console.log(pokeNum);
-
   let findCard = data.pokemon.find(pokemon =>  pokemon.num == pokeNum);
   let boxType = `type-${findCard.type[0]}`;
   contentModal.setAttribute('id', boxType);
-
-  
-  contentModal.innerHTML = `<span class="close">&times;</span><div class="c-modal-header"><img src="${findCard.img}" alt=""><h1>${findCard.name}</h1><h2><strong>Type:</strong> ${findCard.type}</h2></div><div class="c-modal-container"><p><strong>Height:</strong> ${findCard.size.height}</p><p><strong>Weight:</strong> ${findCard.size.weight}</p><p><strong>About:</strong> ${findCard.about}</p></div><div class="c-modal-footer"><p><strong>Quick Move:</strong> ${findCard['quick-move'][0].name}</p><p><strong>Special-Attack:</strong> ${findCard['special-attack'][0].name}</p></div>`;
-
+  contentModal.innerHTML = `
+    <span class="close">&times;</span>
+    <div class="c-modal-header"><img src="${findCard.img}" alt="">
+      <h1>${findCard.name}</h1>
+      <h2><strong>Type:</strong> ${findCard.type}</h2>
+    </div>
+    <div class="c-modal-container">
+      <p><strong>Height:</strong> ${findCard.size.height}</p>
+      <p><strong>Weight:</strong> ${findCard.size.weight}</p>
+      <p><strong>About:</strong> ${findCard.about}</p>
+    </div>
+    <div class="c-modal-footer">
+      <p><strong>Quick Move:</strong> ${findCard['quick-move'][0].name}</p>
+      <p><strong>Special-Attack:</strong> ${findCard['special-attack'][0].name}</p>
+      <p>${findCard.name.toUpperCase()} has base-attack greater than ${getBaseAttackPercentage(data.pokemon, findCard)}% of Pokémons.</p>
+    </div>`;
   let btClose = modal.querySelector('.close');
   btClose.addEventListener('click', hiddenModal);
-
 }
 
 const hiddenModal = () => {
@@ -44,22 +54,10 @@ const hiddenModal = () => {
 
 window.addEventListener('load', () => {
     input.addEventListener("keyup", pokemonSearch);
-
     showCards();
-    
-    let cardsButtons = document.querySelectorAll('.card button');
-
-    cardsButtons.forEach(button => {
-      button.addEventListener('click', showModal);
-    });
-
-    input.addEventListener("keyup", pokemonSearch);
-    
-
 })
 
 const modal = document.querySelector('.c-modal');
-const btnsearch = document.getElementById("btnsearch")
 const input = document.getElementById("search")
 
 function pokemonCardsClear(){
@@ -77,18 +75,25 @@ function pokemonSearch(){
           let cards = document.querySelector('.c-cards');
           let card = document.createElement('section');
           cards.appendChild(card);
-          card.innerHTML = `<article class="card type-${pokemon.type[0]}"><span class='card-number'>${pokemon.num}</span><img src="${pokemon.img}" alt=""><h1>${pokemon.name}</h1><h2>${pokemon.type}</h2><button>Detalhes</button></article>`;
+          card.innerHTML = `<article class="card type-${pokemon.type[0]}" data-num="${pokemon.num}"><span class='card-number'>${pokemon.num}</span><img src="${pokemon.img}" alt=""><h1>${pokemon.name}</h1><h2>${pokemon.type}</h2><button>Detalhes</button></article>`;
+          let cardsButtons = document.querySelectorAll('.card button');
+          cardsButtons.forEach(button => {
+            button.addEventListener('click', showModal);
+          });
         } else if (pokemon.type[1] != undefined){
           if(pokemon.type[1].startsWith(search)){
             let cards = document.querySelector('.c-cards');
             let card = document.createElement('section');
             cards.appendChild(card);
-            card.innerHTML = `<article class="card type-${pokemon.type[0]}"><span class='card-number'>${pokemon.num}</span><img src="${pokemon.img}" alt=""><h1>${pokemon.name}</h1><h2>${pokemon.type}</h2><button>Detalhes</button></article>`;
+            card.innerHTML = `<article class="card type-${pokemon.type[0]}" data-num="${pokemon.num}"><span class='card-number'>${pokemon.num}</span><img src="${pokemon.img}" alt=""><h1>${pokemon.name}</h1><h2>${pokemon.type}</h2><button>Detalhes</button></article>`;
+            let cardsButtons = document.querySelectorAll('.card button');
+            cardsButtons.forEach(button => {
+              button.addEventListener('click', showModal);
+            });
           }
         }
     })
 }
-btnsearch.addEventListener("click", pokemonSearch)
 
 const pokemonOrder = document.getElementById("ordenar")
 pokemonOrder.addEventListener("change", qualquer)
@@ -108,3 +113,5 @@ function qualquer(){
     showCards(ordenarNum(data.pokemon))
   }
 }
+
+console.log()
